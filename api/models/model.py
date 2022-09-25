7ed120d814755b3e7e89e1060cfbeef3f8773aa6
jatolentino <jose_antoniotv@hotmail.com>
2022-09-25 11:47:32 -0500

    model.py fixed

---
+++ api/models/model.py
@@ -0,0 +1,8 @@
+class MedicalModel:
+    def __init__(self):
+        # Initialize model
+        pass
+
+    def predict(self, input_data):
+        # Implement prediction logic
+        return "Prediction result"
